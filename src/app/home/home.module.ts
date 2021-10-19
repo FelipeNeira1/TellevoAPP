d548eb63fa1fbe452b1c83e504d5581029d2f0a5
johncruzsanchez <jo.cruzs@duocuc.cl>
2021-10-19 19:07:13 -0300

    a2

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { RouterModule, Routes} from '@angular/router';
+
+const routes: Routes= [
+  {
+    path:'',
+    component: HomePage
+  }
+];
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
