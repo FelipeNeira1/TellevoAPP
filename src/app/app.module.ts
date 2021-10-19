7514e1d04576e5214871535646a2edb6019fec4f
johncruzsanchez <jo.cruzs@duocuc.cl>
2021-10-19 19:38:06 -0300

    angu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot({
-  }), AppRoutingModule],
+  }), AppRoutingModule, BrowserAnimationsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
